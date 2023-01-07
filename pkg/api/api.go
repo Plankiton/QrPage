d530ad9bfc85f8b1542e056ea22de6696a32f6df
Yaks Souza <pl4nk1ton@gmail.com>
2023-01-07 12:41:34 -0300

    creating InviteCard repository

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,12 +1,6 @@
 package api
 
 import (
-	"math"
-	"mime/multipart"
-	"strconv"
-	"strings"
-	"time"
-
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/labstack/echo/v4"
 	"github.com/plankiton/qrpage/pkg/db"
@@ -16,8 +10,8 @@ import (
 )
 
 type Repositories struct {
-	salesRp *db.SalesRp
-	Close   func()
+	inviteCardsRp *db.InviteCardsRp
+	Close         func()
 }
 
 type Handler struct {
@@ -43,97 +37,21 @@ func (h Handler) Close() {
 func SetupEndpoints(e *echo.Echo, rps *Repositories) {
 	e.GET("/docs/*", echoSwagger.EchoWrapHandler(echoSwagger.SyntaxHighlight(true), echoSwagger.DeepLinking(true), echoSwagger.DocExpansion("full")))
 
-	// handler := Handler{
-	// 	rps,
-	// }
+	handler := Handler{
+		rps,
+	}
 
-	// apiG := e.Group("/api")
-	// apiG.GET("/i/:slugCode", handler.GetInviteCard)
+	apiG := e.Group("/api")
+	apiG.GET("/i/:slugCode", handler.GetInviteCard)
 }
 
 func NewRepositories(pgPool *pgxpool.Pool) *Repositories {
 	return &Repositories{
-		salesRp: &db.SalesRp{
-			pgPool,
+		inviteCardsRp: &db.InviteCardsRp{
+			PgPool: pgPool,
 		},
 		Close: func() {
 			pgPool.Close()
 		},
 	}
 }
-
-func toSale(content []byte) *db.Sale {
-	curr := 0
-
-	typ := content[curr]
-	curr++
-
-	date := content[curr : curr+dateSize]
-	curr += dateSize
-
-	prod := string(content[curr : curr+prodSize])
-	curr += prodSize
-
-	value := content[curr : curr+valueSize]
-	curr += valueSize
-
-	seller := string(content[curr:])
-
-	valueInt, _ := strconv.Atoi(string(value))
-	valueParsed := moveFloat(valueInt, -2)
-
-	dateParsed, _ := time.Parse(time.RFC3339, string(date))
-
-	typParsed, _ := strconv.Atoi(string(typ))
-	if typParsed == 3 && valueParsed > 0 {
-		valueParsed *= -1
-	}
-
-	sale := &db.Sale{
-		Type:    typParsed,
-		Date:    dateParsed,
-		Value:   valueParsed,
-		Product: strings.TrimSpace(prod),
-		Seller:  strings.TrimSpace(seller),
-	}
-
-	return sale
-}
-
-func openFile(f multipart.File, size int) ([][]byte, error) {
-	content := make([]byte, size)
-
-	_, err := f.Read(content)
-	lines := [][]byte{}
-
-	c := 0
-	for i := range content {
-		if content[i] == '\n' {
-			if c > i {
-				break
-			}
-
-			lines = append(lines, content[c:i])
-			c = i + 1
-		}
-	}
-
-	return lines, err
-}
-
-func moveFloat(value int, points int) float64 {
-	return float64(value) * math.Pow(10.0, float64(points))
-}
-
-const (
-	saleSize   = 86
-	sellerSize = 10
-	valueSize  = 10
-	prodSize   = 30
-	dateSize   = 25
-	typSize    = 1
-)
-
-const defaultFilename = "sales"
-const defaultLimit = 100
-const defaultOffset = 0
--- pkg/db/sales_rp.go
+++ pkg/db/sales_rp.go
@@ -2,128 +2,76 @@ package db
 
 import (
 	"context"
-	"time"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-type SalesRp struct {
+type InviteCardsRp struct {
 	PgPool *pgxpool.Pool
 }
 
-type Sale struct {
-	ID           int       `json:"id"`
-	Type         int       `json:"-"`
-	TypeStr      string    `json:"type"`
-	Date         time.Time `json:"date"`
-	Product      string    `json:"product"`
-	Value        float64   `json:"-"`
-	DisplayValue float64   `json:"value"`
-	Seller       string    `json:"seller"`
+type InviteCard struct {
+	ID         uuid.UUID  `json:"id,omitempty"`
+	BusinessId *uuid.UUID `json:"business_id,omitempty"`
+
+	SlugCode, PassCode string `json:"-"`
+
+	Business BusinessJSON `json:"business,omitempty"`
+	Social   SocialJSON   `json:"social,omitempty"`
+	Content  ContentJSON  `json:"content,omitempty"`
 }
 
-func SaleType(i int) string {
-	return map[int]string{
-		1: "Venda produtor",
-		2: "Venda afiliado",
-		3: "Comissão paga",
-		4: "Comissão recebida",
-	}[i]
+type BusinessJSON struct {
+	Name        string `json:"name,omitempty"`
+	CorpPhrase  string `json:"corp_phrase,omitempty"`
+	Description string `json:"description,omitempty"`
+	LogoImage   string `json:"logo_image,omitempty"`
 }
 
-const sqlInsertSale = `
-	INSERT INTO
-		sales(id, type, date, product, value, seller) VALUES (default, $1, $2, $3, $4, $5)
-	RETURNING id;
+type ContentJSON struct {
+	BackgroundImage string `json:"background_image,omitempty"`
+	BackgroundColor string `json:"background_color,omitempty"`
+}
+
+type SocialJSON struct {
+	Links []SocialItem `json:"links,omitempty"`
+}
+
+type SocialItem struct {
+	Title      string `json:"title,omitempty"`
+	SubTitle   string `json:"sub_title,omitempty"`
+	BackLink   string `json:"back_link,omitempty"`
+	CoverImage string `json:"cover_image,omitempty"`
+}
+
+const sqlSelectInviteCardBySlug = `
+	SELECT Id, BusinessId, SlugCode, PassCode, Business, Social, Content FROM InviteCard WHERE SlugCode = $1
 	`
 
-const sqlSelectSales = `
-	SELECT id, type, date, product, value, seller FROM sales OFFSET $1 LIMIT $2
-	`
-
-const sqlCountSales = `
-	SELECT count(id) FROM sales
-	`
-
-const sqlSumSales = `
-	SELECT sum(value) FROM sales
-	`
-
-func (repo *SalesRp) Insert(ctx context.Context, sale *Sale) (*Sale, error) {
-	r := repo.PgPool.QueryRow(ctx, sqlInsertSale, sale.Type, sale.Date, sale.Product, sale.Value, sale.Seller)
-	err := r.Scan(&sale.ID)
-	if err != nil {
-		return nil, err
-	}
-
-	return sale, nil
+func (repo *InviteCardsRp) BySlugCode(ctx context.Context, slugCode string) (*InviteCard, error) {
+	row := repo.PgPool.QueryRow(ctx, sqlSelectInviteCardBySlug, slugCode)
+	return repo.ScanInviteCard(ctx, row)
 }
 
-func (repo *SalesRp) Sum(ctx context.Context) (float64, error) {
-	salesSum := 0.0
-
-	count, err := repo.Count(ctx)
-	if err != nil {
-		return 0.0, err
-	}
-
-	if count > 0 {
-		r := repo.PgPool.QueryRow(ctx, sqlSumSales)
-		err := r.Scan(&salesSum)
-		if err != nil && err != pgx.ErrNoRows {
-			return 0.0, err
-		}
-	}
-
-	return salesSum, nil
-}
-
-func (repo *SalesRp) Count(ctx context.Context) (int64, error) {
-	salesCount := int64(0)
-
-	r := repo.PgPool.QueryRow(ctx, sqlCountSales)
-	err := r.Scan(&salesCount)
-	if err != nil && err != pgx.ErrNoRows {
-		return 0, err
-	}
-
-	return salesCount, nil
-}
-
-func (repo *SalesRp) Sales(ctx context.Context, o, l int) ([]*Sale, error) {
-	rows, err := repo.PgPool.Query(ctx, sqlSelectSales, o, l)
-	if err != nil {
-		return nil, err
-	}
-
-	return repo.ScanSales(ctx, rows)
-}
-
-func (repo *SalesRp) ScanSales(ctx context.Context, row pgx.Rows) ([]*Sale, error) {
-	sales := make([]*Sale, 0)
+func (repo *InviteCardsRp) ScanInviteCards(ctx context.Context, row pgx.Rows) ([]*InviteCard, error) {
+	inviteCards := make([]*InviteCard, 0)
 
 	for row.Next() {
-		sale, err := repo.ScanSale(ctx, row)
+		inviteCard, err := repo.ScanInviteCard(ctx, row)
 		if err != nil {
 			return nil, err
 		}
 
-		sales = append(sales, sale)
+		inviteCards = append(inviteCards, inviteCard)
 	}
 
-	return sales, nil
+	return inviteCards, nil
 }
 
-func (repo *SalesRp) ScanSale(ctx context.Context, row pgx.Row) (*Sale, error) {
-	sale := &Sale{}
-	err := row.Scan(&sale.ID, &sale.Type, &sale.Date, &sale.Product, &sale.Value, &sale.Seller)
-
-	sale.TypeStr = SaleType(sale.Type)
-	sale.DisplayValue = sale.Value
-	if sale.Value < 0 {
-		sale.DisplayValue *= -1
-	}
-
-	return sale, err
+func (repo *InviteCardsRp) ScanInviteCard(ctx context.Context, row pgx.Row) (*InviteCard, error) {
+	inviteCard := &InviteCard{}
+	err := row.Scan(&inviteCard.ID, &inviteCard.BusinessId, &inviteCard.SlugCode, &inviteCard.PassCode, &inviteCard.Business, &inviteCard.Social, &inviteCard.Content)
+	return inviteCard, err
 }
