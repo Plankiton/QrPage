a658634f01ea0affc631d4c5553be1959611e6b4
Yaks Souza <pl4nk1ton@gmail.com>
2023-01-07 12:57:15 -0300

    inprove endpoint response

--- pkg/api/get_invite_card.go
+++ pkg/api/get_invite_card.go
@@ -3,11 +3,20 @@ package api
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
+	"github.com/plankiton/qrpage/pkg/db"
 )
 
+// GetInviteCard Godoc
+// @Summary get a invite card by SlugCode
+// @ID get-invite-card-by-slugcode
+// @Produce json
+// @Param id path string true "todo ID"
+// @Success 200 {object}
+// @Failure 404 {object} message
+// @Router /api/i/{SlugCode} [get]
 func (h *Handler) GetInviteCard(c echo.Context) error {
-	logger := c.Logger()
 	ctx := c.Request().Context()
+	logger := c.Logger()
 
 	slugCode := c.Param("slugCode")
 	inviteCard, err := h.rps.inviteCardsRp.BySlugCode(ctx, slugCode)
@@ -17,14 +26,15 @@ func (h *Handler) GetInviteCard(c echo.Context) error {
 			"err":     err.Error(),
 		})
 
-		return c.JSON(404, echo.Map{
-			"ok":  false,
-			"err": "error searching for inviteCard",
-		})
+		return c.JSON(404, GetInviteCardPayload{Error: "error searching for inviteCard"})
 	}
 
-	return c.JSON(200, echo.Map{
-		"ok":   true,
-		"data": inviteCard,
+	return c.JSON(200, GetInviteCardPayload{
+		Data: inviteCard,
 	})
+}
+
+type GetInviteCardPayload struct {
+	Data  *db.InviteCard `json:"data"`
+	Error string         `json:"error,omitempty"`
 }
